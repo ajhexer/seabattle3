9e34afb4cf8dddb9e0002174809d6fc93688b130
ajhexer <alirezajelokhani559@yahoo.com>
2021-02-05 18:51:32 +0330

    first inserts

---
+++ seabattle/cmake-build-debug/CMakeFiles/seabattle.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/seabattle.dir/main.c.obj"
+  "libseabattle.dll.a"
+  "seabattle.exe"
+  "seabattle.exe.manifest"
+  "seabattle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/seabattle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
